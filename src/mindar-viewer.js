86327ae038de26b48de46c125a8f0804ee334620
taicancr7 <taicancr7@gmail.com>
2022-10-26 09:09:53 +0700

    update

--- src/mindar-viewer.js
+++ src/mindar-viewer.js
@@ -56,8 +56,8 @@ const MindARViewer = (props) => {
             <a-plane
               src={`#${item.id}`}
               position="0.3 0.2 0"
-              height="0.8"
-              width="1.4"
+              height="0.6"
+              width="1.2"
               rotation="0 0 0"
             ></a-plane>
           </a-entity>
